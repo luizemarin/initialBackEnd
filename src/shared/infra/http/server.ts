e7378bdbfde5472b85722055d60bbf515428c554
Luiz <luizemarin@gmail.com>
2020-12-31 09:51:51 -0300

    Validate routes with body and params requests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Response, Request, NextFunction } from 'express';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import 'express-async-errors';
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     if (err instanceof AppError) {
